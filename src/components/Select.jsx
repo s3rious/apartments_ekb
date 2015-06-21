e06c7c50dd6a5b6ce8bcb6f8d885e90643b1df82
s3rious a.k.a Leonid Semenov <canismajor@kaschenka.ru>
2015-06-21 20:21:28 +0500

    Select component have arrived

---
+++ src/components/Select.jsx
@@ -0,0 +1,74 @@
+import React from 'react/addons';
+
+import _ from 'lodash';
+import classnames from 'classnames/dedupe';
+
+class Select extends React.Component {
+
+  static propTypes = {
+    name: React.PropTypes.string.isRequired,
+    label: React.PropTypes.string.isRequired,
+    payload: React.PropTypes.array.isRequired,
+    disabled: React.PropTypes.bool
+  }
+
+  handleChange (selectedValue) {
+    let value = selectedValue.target.value;
+    let selectedOption = _.findWhere(this.props.payload, { name: value });
+
+    let payload = _.clone(this.props.payload);
+
+    payload.forEach((option) => {
+      option.active = option.name === selectedOption.name;
+    });
+
+    this.props.onChange({
+      [this.props.name]: payload
+    });
+  }
+
+  render () {
+
+    let options = _(this.props.payload)
+      .chain()
+      .map(option => {
+
+        return (
+          <option
+            key={ option.name }
+          >
+            { option.name }
+          </option>
+        );
+      })
+      .value();
+
+    let classes = classnames({
+      'Select': true,
+      'Select--disabled': this.props.disabled
+    });
+
+    return (
+
+      <fieldset
+        className={ classes }
+        disabled={this.props.disabled || false}
+      >
+        <label>
+          <span className="Select-label">
+            { this.props.label }
+          </span>
+          <select
+            className="Select-select"
+            onChange={ this.handleChange.bind(this) }
+            defaultValue={ _.findWhere(this.props.payload, { active: true }) }
+          >
+            { options }
+          </select>
+        </label>
+      </fieldset>
+    )
+  }
+}
+
+export default Select;
